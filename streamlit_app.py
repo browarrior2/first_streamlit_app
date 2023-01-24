0187791969d17e8bda44e96bf7ce2f22780006ee
browarrior2 <123408209+browarrior2@users.noreply.github.com>
2023-01-24 14:02:50 -0600

    Update streamlit_app.py
    made output more user freindly

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# makes the json user friendly
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#displays the friendly table
+streamlit.dataframe(fruityvice_normalized)
